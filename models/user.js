0923507aa08fe4249eee02f4c45eca16701e8943
fadlan7 <inifadlan7@gmail.com>
2022-10-15 19:23:38 +0700

    custom message sequelize validation

--- models/user.js
+++ models/user.js
@@ -17,44 +17,53 @@ module.exports = (sequelize, DataTypes) => {
       full_name: {
         type: DataTypes.STRING,
         validate: {
-          notEmpty: true,
+          notEmpty: { args: true, msg: 'Full name is required' },
         },
       },
       email: {
         type: DataTypes.STRING,
-        unique: true,
+        unique: {
+          args: true,
+          msg: 'This email has been used, try another one',
+        },
         validate: {
-          isEmail: true,
-          notEmpty: true,
+          isEmail: { args: true, msg: 'Invalid email address' },
+          notEmpty: { args: true, msg: 'Email is required' },
         },
       },
       username: {
         type: DataTypes.STRING,
-        unique: true,
+        unique: {
+          args: true,
+          msg: 'This username has been used, try another one',
+        },
         validate: {
-          notEmpty: true,
+          notEmpty: { args: true, msg: 'Username is required' },
         },
       },
-      password: { type: DataTypes.STRING, validate: { notEmpty: true } },
+      password: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: { args: true, msg: 'Password is required' } },
+      },
       profile_image_url: {
         type: DataTypes.TEXT,
         validate: {
-          notEmpty: true,
-          isUrl: true,
+          notEmpty: { args: true, msg: 'Profile image url is required' },
+          isUrl: { args: true, msg: 'Invalid URL format' },
         },
       },
       age: {
         type: DataTypes.INTEGER,
         validate: {
-          notEmpty: true,
-          isInt: true,
+          notEmpty: { args: true, msg: 'Age is required' },
+          isInt: { args: true, msg: 'Is not a number on age' },
         },
       },
       phone_number: {
         type: DataTypes.INTEGER,
         validate: {
-          notEmpty: true,
-          isInt: true,
+          notEmpty: { args: true, msg: 'Age is required' },
+          isInt: { args: true, msg: 'Is not a number on phone number' },
         },
       },
     },
